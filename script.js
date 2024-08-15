fe0b11fa7d7c8ff2541ff5fdb6bedb5f81380790
Arifals <halo.langit21@gmail.com>
2024-08-15 20:54:59 +0700

    tambah capaian dan tujuan

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const closePetunjuk = () => {
   const guides = document.getElementById("petunjuk");
   guides.classList.remove("show");
 };
+const showCandt = () => {
+  const guides = document.getElementById("candt");
+  guides.classList.add("show");
+};
+const closeCandt = () => {
+  const guides = document.getElementById("candt");
+  guides.classList.remove("show");
+};
